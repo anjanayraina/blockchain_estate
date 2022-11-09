2747538247598ec8298a4c7de5e10320a5c13978
Anjanay Raina <anjanayraina326@gmail.com>
2022-11-09 17:00:33 +0530

    Comments Added

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -7,11 +7,15 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract RealEstate is ERC721URIStorage {
     using Counters for Counters.Counter;
-    Counters.Counter private allTokens;
+    // this is used as a sort of inheritance on the vairable that you are assigening the Counters class 
+    // so Counters will get assigned all the functoins of the Class Counters 
+    // these inbuilt libraries give us the implementatoin with audited code 
+    
+    Counters.Counter private tokenCounter;
     constructor() ERC721("Land NFT" , "RNFT"){}
     function mint(string calldata tokenURI) public returns (uint256){
-        allTokens.increment();
-        uint256 newLand = allTokens.current();
+        tokenCounter.increment();
+        uint256 newLand = tokenCounter.current();
         _mint(msg.sender ,newLand);
         _setTokenURI(newLand , tokenURI);
         return newLand;
@@ -19,7 +23,7 @@ contract RealEstate is ERC721URIStorage {
 
     }
     function getSupply() public view returns (uint256){
-        return allTokens.current();
+        return tokenCounter.current();
         
     }
 
